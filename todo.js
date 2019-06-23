cb346adee000f95a7187158fd8611d6bcbf3c893
luminousbeam <jocelyn.jeriah@gmail.com>
2019-06-22 23:21:47 -0400

    add initial vanilla JS logic

---
+++ todo.js
@@ -0,0 +1,14 @@
+//a place to store todos
+var todos = ["item 1", "item 2", "item 3", "item 4"]
+
+//a way to display todos
+console.log("My Todos:", todos)
+
+//a way to add new todos
+todos.push("item 5")
+
+//a way to change a todo
+todos[0] = "item 1 updated"
+
+//a way to delete a todo
+todos.splice(0,1)
